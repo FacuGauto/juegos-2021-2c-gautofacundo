ae2d13afde0679edb1885350f82d95f00477b16a
FacuGauto <fgauto1992@gmail.com>
2021-09-02 19:13:27 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './page/error/error.component';
+import { LoginComponent } from './page/login/login.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
