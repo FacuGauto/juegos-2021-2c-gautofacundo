c196bd8cf72ce52f66b7e9c0e7d17911652b15fe
FacuGauto <fgauto1992@gmail.com>
2021-09-02 20:41:36 -0300

    quine soy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './page/error/error.component';
 import { HomeComponent } from './page/home/home.component';
 import { LoginComponent } from './page/login/login.component';
+import { QuienSoyComponent } from './page/quien-soy/quien-soy.component';
 
 const routes: Routes = [
-  {path:'',redirectTo: 'home', pathMatch: 'full'},
+  {path:'',redirectTo: 'login', pathMatch: 'full'},
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'casa',component:HomeComponent},
+  {path:'quien-soy',component:QuienSoyComponent},
   {path:'**', component: ErrorComponent}
 ];
 
